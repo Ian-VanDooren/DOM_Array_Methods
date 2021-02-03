85237eecdd40332405106e041e044ba4a1361c98
Ian VanDooren <63620074+Ian-VanDooren@users.noreply.github.com>
2021-02-03 08:37:42 -0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+Wealth tracker created using vanilla JS, CSS and HTML following Traversy Media tutorial. Incorporates forEach, map,
+filter, sort and reduce to manipulate the DOM
--- script.js
+++ script.js
@@ -27,6 +27,38 @@ async function getRandomUser(){
    addData(newUser);
 }
 
+//Double everyones money
+function doubleMoney () {
+    data = data.map(user => {
+        return {...user, money: user.money * 2};
+    });
+
+    updateDom();
+}
+
+//Sort users by richest
+function sortByRichest () {
+    data.sort((a, b) => b.money - a.money);
+
+    updateDom();
+}
+
+//Filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDom();
+}
+
+//Calculate the total wealth
+function calculateWealth () {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 //Add new obj to data arr
 function addData(obj){
     data.push(obj);
@@ -44,12 +76,21 @@ function updateDom(providedData = data){
         const element = document.createElement('div');
         element.classList.add('person');
         element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+        {formatMoney(item.money)};
         main.appendChild(element);
 
     });
 }
 
 //Format number as money
-function formatMoney() {
-    
+function formatMoney(number) {
+    return'$'+number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
+
+//Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
+
